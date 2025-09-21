cb77a95ab8162554e610dd6d5863c9da7951e337
sarah <sarahlakrouz@gmail.com>
2025-09-21 12:13:19 +0200

    questions 1,2,3,4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {Liste} from "./liste/liste";
+import {Gestion} from "./gestion/gestion";
+import {Accueil} from "./accueil/accueil";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Accueil },
+  { path: 'liste', component: Liste},
+  { path: 'accueil', component: Accueil},
+  { path: 'gestion', component: Gestion},
+];
